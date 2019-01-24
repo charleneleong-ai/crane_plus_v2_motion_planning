87e6a3ed9ac44e3f4d377951cf85cb30da819aaf
Charlene Leong <charlene.leong@zn.mitsubishielectric.co.jp>
2019-01-24 14:00:00 +0900

    [feat] Calling smac3_run.py from smac session

--- crane_plus_control/scripts/modules/smac_session.py
+++ crane_plus_control/scripts/modules/smac_session.py
@@ -3,11 +3,12 @@
 # File Created: Friday, January 18th 2019, 1:36:24 pm
 # Author:  Charlene Leong (charleneleong84@gmail.com>)
 # Modified By: Charlene Leong
-# Last Modified: Thursday, January 24th 2019, 11:16:13 am
+# Last Modified: Thursday, January 24th 2019, 11:59:42 am
 ###
 
 import sys
 import os
+import subprocess
 import numpy as np
 
 import rospkg
@@ -76,76 +77,10 @@ class SMACSession(Session):
             # self._write_pcs(planner, params_set, pcs_fp)
             # self._write_scenario(planner, self.scenes[0], scenario_fp, pcs_fp)
 
-            os.system('cd '+ROS_PKG_PATH +'/smac && ./smac --scenario-file '+scenario_fp+' --seed 123')
+            # os.system('cd '+ROS_PKG_PATH +'/smac && ./smac --scenario-file '+scenario_fp+' --seed 123')
 
+            smac3_run = ROS_PKG_PATH+"/smac3_run.py"  
 
-# def run():
-#     # for planner, params_set in planner_config.iteritems():
-#     ROS_PKG_PATH = '.'
-#     planner = 'RRTConnectkConfigDefault'
-#     params_set = {'default': [0.1, 2.0, 0.1], 'type': 'geometric::RRTConnect'}
-#     planner_select = 'Cano_etal'
-#     scene = ('')
-
-
-#     pcs_fp = ROS_PKG_PATH+'/smac/scenarios/'+planner + \
-#         '/'+planner_select+'_'+planner+'.pcs'
-#     scenario_fp = ROS_PKG_PATH+'/smac/scenarios/'+planner+'/'+planner+'_scenario.txt'
-
-#     write_pcs(planner, params_set, pcs_fp)
-#     write_scenario(planner, scene[0], scenario_fp, pcs_fp)
-
-#     os.system('cd '+ROS_PKG_PATH +
-#                 '/smac && ./smac --scenario-file '+scenario_fp+' --seed 123')
-
-# def _write_pcs(planner, params_set, pcs_fp):
-#     try:
-#         pcs_file = open(pcs_fp, 'w')
-#         for k, v in params_set.iteritems():
-#             if isinstance(v, list):
-#                 if(planner_select == "Cano_etal"):
-#                     param_range = ",".join(
-#                         [str(round(x, 2)) for x in np.arange(v[0], v[1], v[2])])
-#                     param_default = str(
-#                         planner_configs_default[planner][k])
-#                     pcs_file.write(
-#                         k + ' categorical [' + param_range + '] ['+param_default+']\n')
-#                 elif(planner_select == "Burger_etal"):
-#                     pass
-#         pcs_file.close()
-#         rospy.logerr('Successfully written pcs_file file. \n%s.', pcs_fp)
-#     except IOError:
-#         rospy.logerr('Error writing pcs file \n%s.', pcs_fp)
-#         sys.exit(1)
-
-# def _write_scenario(planner, scene, scenario_fp, pcs_fp):
-#     try:
-#         scenario_file = open(scenario_fp, 'w')
-#         scenario_file.write('use-instances = false\n')
-#         scenario_file.write('numberOfRunsLimit = ' +
-#                             str(max_trials) + '\n')
-#         scenario_file.write('runtimeLimit = ' +
-#                             str(max_runtime) + '\n')
-#         scenario_file.write('runObj = QUALITY\n')
-#         scenario_file.write('deterministic = 1\n')
-#         scenario_file.write('pcs-file = ' + pcs_fp + '\n')
-#         scenario_file.write('algo = python smac_run.py ' +
-#                             planner + ' ' + scene + ' ' + n_trial + '\n')
-#         scenario_file.write('check-sat-consistency false \n')
-#         scenario_file.write('check-sat-consistency-exception false \n')
-#         scenario_file.write('algo-cutoff-time 40\n')
-#         scenario_file.write('kill-run-exceeding-captime-factor 2\n')
-#         # scenario_file.write('kill-run-exceeding-captime false')
-#         # scenario_file.write('transform-crashed-quality false')
-#         scenario_file.close()
-#         rospy.logerr(
-#             'Successfully written scenario file. \n%s', scenario_fp)
-#     except IOError:
-#         rospy.logerr('Error writing scenario file. \n%s', scenario_fp)
-#         sys.exit(1)
-
-# scene = ["narrow_passage"]
-
-# if __name__ == "__main__":
-#     rospy.init_node('smac_session', anonymous=True)
-#     run()
+            process = subprocess.Popen(smac3_run.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate() 
+            print(output)
